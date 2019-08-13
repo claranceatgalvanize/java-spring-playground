de3371871d34cb1bcd3b83cc1eaab16b5119b3ef
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-08-13 12:09:40 -0700

    My first spring playground commit.

---
+++ src/main/java/com/galvanize/javaspringbootplayground/JavaSpringBootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.javaspringbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootPlaygroundApplication.class, args);
+	}
+
+}
